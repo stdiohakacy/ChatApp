81c2f966d1eb24d008c4825c69822b3eb36135d6
Duy <huynhduytam2210@gmail.com>
2020-03-11 00:40:07 +0700

    done

--- server/index.js
+++ server/index.js
@@ -2,71 +2,51 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
-const {addUser, removeUser, getUser, getUserInRoom} = require('./users');
-
 const app = express();
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+app.get("/", (req, res) => {
+    res.send({ response: "Server is up and running." }).status(200);
+});
+
 const server = http.createServer(app);
 const io = socketio(server);
+
 app.use(cors());
 
 io.on('connect', (socket) => {
-    // join
-    socket.on('join', ({name, room}, callback) => {
-        const {error, user} = addUser({id: socket.id, name, room});
-        if(error) {
-            return callback(error);
-        }
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-        socket.join(user.room);
+    if(error) return callback(error);
 
-        // norification itseft
-        socket.emit('message', {
-            user: 'admin', 
-            text: `${user.name} welcome to room ${user.room}`
-        });
+    socket.join(user.room);
 
-        // notification broadcast
-        socket.broadcast.emit('roomData', {
-            room: user.room,
-            users: getUserInRoom(user.room)
-        })
-    
-        callback();
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
-    });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
-    // send message
-    socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id);
-        io.to(user.room).emit('message', {
-            user: user.name,
-            text: message
-        });
-        callback();
-    });
+    callback();
+  });
 
-    // disconnect
-    socket.on('disconnect', () => {
-        const user = removeUser(socket.id);
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
 
-        if(user) {
-            io.to(user.room).emit('message', {
-                user: 'Admin',
-                text: `${user.name} has left`
-            });
+    io.to(user.room).emit('message', { user: user.name, text: message });
 
-            io.to(user.room).emit('roomData', {
-                room: user.room,
-                users: getUserInRoom(user.room)
-            });
-        }
-    });
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
 });
 
-app.get('/', (req, res) => {
-    res.send({message: 'Hello data'}).status(200);
-});
-
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server has started at port ${process.env.PORT || 3000}`);
-});
+server.listen(process.env.PORT || 4000, () => console.log(`Server has started.`));
